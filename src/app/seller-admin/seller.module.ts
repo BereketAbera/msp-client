05b516c4d3718a78c8fc3ef17994f547e25f8dbe
bereket-abera <bereket5042@gmail.com>
2020-03-14 09:39:46 +0000

    image crop half way

--- src/app/seller-admin/seller.module.ts
+++ src/app/seller-admin/seller.module.ts
@@ -1,31 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms'
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
+import { SharedModule } from "../shared/shared.module";
 
-import {SharedModule} from '../shared/shared.module';
+import { SellerRoutingModule } from "./seller-routing.module";
+import { SellerAdminComponent } from "./seller-admin.component";
+import { AddNewAdComponent } from "./add-new-ad/add-new-ad.component";
+import { NewShopComponent } from "./new-shop/new-shop.component";
+import { ShopListComponent } from "./shop-list/shop-list.component";
+import { SellerDashboardComponent } from "./seller-dashboard/seller-dashboard.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { ProductsComponent } from "./products/products.component";
+import { TransactionsComponent } from "./transactions/transactions.component";
+import { QrScannerComponent } from "./qr-scanner/qr-scanner.component";
 
-import {SellerRoutingModule} from './seller-routing.module';
-import { SellerAdminComponent } from './seller-admin.component';
-import {AddNewAdComponent} from './add-new-ad/add-new-ad.component';
-import {NewShopComponent} from './new-shop/new-shop.component';
-import {ShopListComponent} from './shop-list/shop-list.component';
-import {SellerDashboardComponent} from './seller-dashboard/seller-dashboard.component';
-import {NavigationComponent} from  './navigation/navigation.component';
-import { ProductsComponent } from './products/products.component';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
-
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
-import { RequestConfirmationComponent } from './request-confirmation/request-confirmation.component';
-import { RequestResultComponent } from './request-result/request-result.component';
-import { SellerOrderDetailComponent } from './seller-order-detail/seller-order-detail.component';
-import { RequestCodeConfirmationComponent } from './request-code-confirmation/request-code-confirmation.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { UploadImgComponent } from './upload-img/upload-img.component';
-import { NewOffPeakProductComponent } from './new-off-peak-product/new-off-peak-product.component';
-import { SalesSummaryComponent } from './sales-summary/sales-summary.component';
-import { DragDropDirective  } from '../service/drag-drop.directive'
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from "ng-pick-datetime";
+import { RequestConfirmationComponent } from "./request-confirmation/request-confirmation.component";
+import { RequestResultComponent } from "./request-result/request-result.component";
+import { SellerOrderDetailComponent } from "./seller-order-detail/seller-order-detail.component";
+import { RequestCodeConfirmationComponent } from "./request-code-confirmation/request-code-confirmation.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { UploadImgComponent } from "./upload-img/upload-img.component";
+import { NewOffPeakProductComponent } from "./new-off-peak-product/new-off-peak-product.component";
+import { SalesSummaryComponent } from "./sales-summary/sales-summary.component";
+import { DragDropDirective } from "../service/drag-drop.directive";
+import { ImageCropperModule } from "ngx-image-cropper";
 
 @NgModule({
   imports: [
@@ -34,7 +34,8 @@ import { DragDropDirective  } from '../service/drag-drop.directive'
     SharedModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
-    SellerRoutingModule
+    SellerRoutingModule,
+    ImageCropperModule
   ],
   declarations: [
     NavigationComponent,
@@ -55,12 +56,11 @@ import { DragDropDirective  } from '../service/drag-drop.directive'
     NewOffPeakProductComponent,
     SalesSummaryComponent,
     DragDropDirective
-    
   ],
-  entryComponents:[
+  entryComponents: [
     RequestConfirmationComponent,
     RequestCodeConfirmationComponent,
     RequestResultComponent
   ]
 })
-export class SellerModule { }
+export class SellerModule {}
--- src/app/seller-admin/upload-img/upload-img.component.ts
+++ src/app/seller-admin/upload-img/upload-img.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit,ViewChild} from '@angular/core';
-import { FormBuilder, Validators, FormControl } from '@angular/forms'
-import { Router, ActivatedRoute } from '@angular/router';
-import { MatDialog } from '@angular/material';
-import { debounceTime, distinctUntilChanged} from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, Validators, FormControl } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { MatDialog } from "@angular/material";
+import { debounceTime, distinctUntilChanged } from "rxjs/operators";
+import { MatSnackBar } from "@angular/material";
 
-import {Picture} from '../../model/picture';
-import {UploadService} from '../../service/upload.service';
+import { Picture } from "../../model/picture";
+import { UploadService } from "../../service/upload.service";
 
-import { SaveConfirmationDialogComponent } from '../../shared/save-confirmation-dialog/save-confirmation-dialog.component';
-import { SaveProgressComponent } from '../../shared/save-progress/save-progress.component';
+import { SaveConfirmationDialogComponent } from "../../shared/save-confirmation-dialog/save-confirmation-dialog.component";
+import { SaveProgressComponent } from "../../shared/save-progress/save-progress.component";
+import { ImageCroppedEvent } from "ngx-image-cropper";
+import Compressor from "compressorjs";
 
 @Component({
-  selector: 'app-upload-img',
-  templateUrl: './upload-img.component.html',
-  styleUrls: ['./upload-img.component.scss']
+  selector: "app-upload-img",
+  templateUrl: "./upload-img.component.html",
+  styleUrls: ["./upload-img.component.scss"]
 })
 export class UploadImgComponent implements OnInit {
+  @ViewChild("file") file;
 
-  @ViewChild('file') file;
+  imageChangedEvent: any = "";
+  croppedImage: any = "";
+  selectedImage;
+  tempImg: any;
 
   showError: boolean = false;
   errors = [];
@@ -26,73 +32,93 @@ export class UploadImgComponent implements OnInit {
   showImage: boolean = false;
   uploadForm = this.fb.group({
     name: ["", Validators.required],
-    img: null,
-
+    img: null
   });
-  constructor(private uploadService:UploadService, public snackBar: MatSnackBar, public dialog: MatDialog, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) { }
- 
-  ngOnInit() {
-  }
+  loadingFile = false;
+
+  constructor(
+    private uploadService: UploadService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {}
   gotoGallery() {
     this.router.navigate(["../"], { relativeTo: this.route });
   }
-  chackFormValidity(){
+  chackFormValidity() {
     return true;
   }
   onSubmit() {
-    
     if (this.file.nativeElement.files.length > 0) {
-      var fileize = +((this.file.nativeElement.files[0].size/1024)/1024).toFixed(4);
-      if(fileize <= 5){
-       if (this.chackFormValidity()) {
-        let picture = new Picture();
-        picture = { ...this.uploadForm.value };
-        console.log({...this.uploadForm.value})
-        console.log(this.file.nativeElement.files)
-        const dialogRef = this.dialog.open(SaveConfirmationDialogComponent, {
-          width: '300px',
-          height: '200px',
-          data: { title: "", question: "do you want to upload this Image?" }
-        });
-        dialogRef.afterClosed().subscribe(result => {
-          if (result == "yes") {
-            const progressDialogRef = this.dialog.open(SaveProgressComponent, {
-              width: '160px',
-              height:'180px',
-              data: { title: "", question: "" }
-            });
-            this.uploadService.createImage(picture, this.file.nativeElement.files).subscribe(
-              res => {
-                if (res['success']) {
-                  progressDialogRef.close();
-                  let snackBarRef = this.snackBar.open("Successfuly Uploaded File", "Upload More", {
-                    duration: 2000,
-                  });
-                  snackBarRef.afterDismissed().subscribe(() => {
-                    this.router.navigate(["../"], { relativeTo: this.route });
-                  });
-                  snackBarRef.onAction().subscribe(() => {
-                    this.showImage = false;
-                    this.uploadForm.reset();
-                  });
-                  //this.router.navigate(["../"], { relativeTo: this.route });
-                } else {
-                  progressDialogRef.close();
-                  this.showError = true;
-                  this.errors = res['messages'];
+      var fileize = +(
+        this.file.nativeElement.files[0].size /
+        1024 /
+        1024
+      ).toFixed(4);
+      if (fileize <= 5) {
+        if (this.chackFormValidity()) {
+          let picture = new Picture();
+          picture = { ...this.uploadForm.value };
+          console.log({ ...this.uploadForm.value });
+          console.log(this.file.nativeElement.files);
+          const dialogRef = this.dialog.open(SaveConfirmationDialogComponent, {
+            width: "300px",
+            height: "200px",
+            data: { title: "", question: "do you want to upload this Image?" }
+          });
+          dialogRef.afterClosed().subscribe(result => {
+            if (result == "yes") {
+              const progressDialogRef = this.dialog.open(
+                SaveProgressComponent,
+                {
+                  width: "160px",
+                  height: "180px",
+                  data: { title: "", question: "" }
                 }
-              },
-              err => {
-                progressDialogRef.close();
-              }
-            );
-          }
-        });
-
+              );
+              this.uploadService
+                .createImage(picture, this.file.nativeElement.files)
+                .subscribe(
+                  res => {
+                    if (res["success"]) {
+                      progressDialogRef.close();
+                      let snackBarRef = this.snackBar.open(
+                        "Successfuly Uploaded File",
+                        "Upload More",
+                        {
+                          duration: 2000
+                        }
+                      );
+                      snackBarRef.afterDismissed().subscribe(() => {
+                        this.router.navigate(["../"], {
+                          relativeTo: this.route
+                        });
+                      });
+                      snackBarRef.onAction().subscribe(() => {
+                        this.showImage = false;
+                        this.uploadForm.reset();
+                      });
+                      //this.router.navigate(["../"], { relativeTo: this.route });
+                    } else {
+                      progressDialogRef.close();
+                      this.showError = true;
+                      this.errors = res["messages"];
+                    }
+                  },
+                  err => {
+                    progressDialogRef.close();
+                  }
+                );
+            }
+          });
+        }
+      } else {
+        alert("Image size is too big");
       }
-    }else{
-      alert("Image size is too big");
-    }
     } else {
       alert("Please select image");
     }
@@ -101,14 +127,13 @@ export class UploadImgComponent implements OnInit {
     this.showError = false;
   }
   filePreviewHandler(event) {
-
     var input = event.target;
     var reader = new FileReader();
     reader.onload = () => {
       //var dataURL = reader.result;
       this.imgPath = reader.result;
       let file = event.target.files[0];
-      this.uploadForm.get('img').setValue({
+      this.uploadForm.get("img").setValue({
         filetype: file.type,
         value: reader.result
       });
@@ -117,4 +142,67 @@ export class UploadImgComponent implements OnInit {
     reader.readAsDataURL(input.files[0]);
   }
 
+  fileChangeEvent(event: any): void {
+    this.loadingFile = true;
+    this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    if (event.width < 550 || event.height < 440) {
+      console.log("image not large enough");
+    }
+    // console.log(event);
+    // this.croppedImage = event;
+  }
+
+  imageLoaded() {
+    // show cropper
+  }
+  cropperReady() {
+    // cropper ready
+  }
+  loadImageFailed() {
+    // show message
+  }
+
+  closeModal() {
+    this.imageChangedEvent = "";
+  }
+
+  imageLoad(event) {
+    // if (event.width < 550 || event.height < 440) {
+    //   this.closeModal();
+    // }
+  }
+
+  saveImage() {
+    this.tempImg = this.croppedImage.base64;
+    let byteCharacters = atob(this.tempImg.split(",")[1]);
+    let byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    let byteArray = new Uint8Array(byteNumbers);
+    let blob = new Blob([byteArray], { type: "image/jpeg" });
+
+    this.compressImage(blob);
+  }
+
+  compressImage(image) {
+    let setLocal = this.setLocalImage;
+    new Compressor(image, {
+      quality: 0.7,
+      success(result) {
+        setLocal(result);
+      },
+      error(err) {
+        console.log(err);
+      }
+    });
+  }
+
+  setLocalImage(blob) {
+    var urlCreator = window.URL;
+    var imageUrl = urlCreator.createObjectURL(blob);
+    document.querySelector("#localImage")["src"] = imageUrl;
+  }
 }
