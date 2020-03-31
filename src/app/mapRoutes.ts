f71c7ce3a410b706cde74347bc4848d4ece78058
bereket-abera <bereket5042@gmail.com>
2020-03-31 11:44:16 +0000

    m-access

--- src/app/mapRoutes.ts
+++ src/app/mapRoutes.ts
@@ -37,5 +37,15 @@ export let mapRoutes = {
 
   "/tlgu-slr": "View Summary",
   "/tlgu-slr/gallery": "Manage Gallery",
-  "/tlgu-slr/gallery/upldimg": "Manage Gallery"
+  "/tlgu-slr/gallery/upldimg": "Manage Gallery",
+
+  "/tlgu-slr/shops": "Manage Shop",
+  "/tlgu-slr/shops/newshp": "Manage Shop",
+
+  "/tlgu-slr/qr-scanner": "Process Buyer Code",
+
+  "/tlgu-slr/trnsctns": "View Order",
+  "/tlgu-slr/trnsctns/id": "View Order",
+
+  "/tlgu-slr/slssmry": "View Summary"
 };
