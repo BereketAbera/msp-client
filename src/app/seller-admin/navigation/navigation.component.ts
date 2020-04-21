fce3d64129c5d452049731fa3668b2ce5f5a50f2
bereket-abera <bereket5042@gmail.com>
2020-04-21 15:59:58 +0000

    navigation s style

--- src/app/seller-admin/navigation/navigation.component.ts
+++ src/app/seller-admin/navigation/navigation.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from "../../service/auth.service";
 @Component({
   selector: "app-navigation",
   templateUrl: "./navigation.component.html",
-  styleUrls: ["./navigation.component.scss"]
+  styleUrls: ["./navigation.component.scss"],
 })
 export class NavigationComponent implements OnInit {
   background = "primary";
