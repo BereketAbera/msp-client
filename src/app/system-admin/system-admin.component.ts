17e066618fdde12c589609f01375478dc6db4890
bereket-abera <bereket5042@gmail.com>
2020-05-08 13:49:20 +0000

    error fix

--- src/app/system-admin/system-admin.component.ts
+++ src/app/system-admin/system-admin.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { environment } from "../../environments/environment";
 
 @Component({
-  selector: 'app-system-admin',
-  templateUrl: './system-admin.component.html',
-  styleUrls: ['./system-admin.component.scss']
+  selector: "app-system-admin",
+  templateUrl: "./system-admin.component.html",
+  styleUrls: ["./system-admin.component.scss"],
 })
 export class SystemAdminComponent implements OnInit {
+  today = new Date().getFullYear();
+  version = environment.version;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
