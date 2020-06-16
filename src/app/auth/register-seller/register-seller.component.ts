b1e7885594dabb36657ad15805709040d1a85556
bereket-abera <bereket5042@gmail.com>
2020-06-16 14:08:31 +0000

    phone number fix

--- src/app/auth/register-seller/register-seller.component.ts
+++ src/app/auth/register-seller/register-seller.component.ts
@@ -119,7 +119,7 @@ export class RegisterSellerComponent implements OnInit {
         return this.userService
           .registerUser({
             ...this.registrationForm.value,
-            phoneNumber:"+251931644114"
+            phoneNumber: this.phoneChangeFormat(phoneNumber.value, "db"),
           })
           .subscribe((res) => {
             window.scrollTo(0, 0);
