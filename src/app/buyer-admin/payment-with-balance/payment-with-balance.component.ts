588437a2c45967bcf1f9e7ff90f691764218badb
bereket-abera <bereket5042@gmail.com>
2020-07-25 09:01:51 +0000

    address name

--- src/app/buyer-admin/payment-with-balance/payment-with-balance.component.ts
+++ src/app/buyer-admin/payment-with-balance/payment-with-balance.component.ts
@@ -38,12 +38,12 @@ export class PaymentWithBalanceComponent implements OnInit {
   paymentForm = this.fb.group({
     deposit: ["", Validators.required],
     paymentType: [this.PAY_WITH_BALANCE, Validators.required],
-    takeOut: [false],
-    specialRequirements: [""],
-    phoneNumber: [
-      "",
-      [Validators.required, Validators.pattern(/(\(\d{3}\))(\s)\d{3}(-)\d{4}/)],
-    ],
+    // takeOut: [false],
+    // specialRequirements: [""],
+    // phoneNumber: [
+    //   "",
+    //   [Validators.required, Validators.pattern(/(\(\d{3}\))(\s)\d{3}(-)\d{4}/)],
+    // ],
   });
 
   prevValue = "";
@@ -63,14 +63,14 @@ export class PaymentWithBalanceComponent implements OnInit {
     this.orderAmount = +this.data.total.toFixed(2);
     this.creditInfoAvailable = this.data.savedCreditCard;
     this.minimumAmount = (this.orderAmount - this.balance).toFixed(2);
-    this.paymentForm.controls["phoneNumber"].setValue(
-      this.phoneChangeFormat(this.data.phoneNumber, "form")
-    );
+    // this.paymentForm.controls["phoneNumber"].setValue(
+    //   this.phoneChangeFormat(this.data.phoneNumber, "form")
+    // );
     //this.paymentForm.get('deposit').setValue(this.minimumAmount.toFixed(2));
 
-    this.paymentForm.controls["phoneNumber"].valueChanges
-      .pipe((debounceTime(200), switchMap((term) => of(term))))
-      .subscribe((res) => this.phoneNumberChange(res));
+    // this.paymentForm.controls["phoneNumber"].valueChanges
+    //   .pipe((debounceTime(200), switchMap((term) => of(term))))
+    //   .subscribe((res) => this.phoneNumberChange(res));
   }
   payWithCredit() {
     if (this.creditInfoAvailable) this.payWith.emit(this.PAY_WITH_SAVED_CARD);
@@ -100,32 +100,32 @@ export class PaymentWithBalanceComponent implements OnInit {
     });
   }
   onSubmit() {
-    this.showError = false;
-    this.errors = [];
+    // this.showError = false;
+    // this.errors = [];
     if (this.cartService.isCartExpired()) {
       this.showResetDialog();
     } else {
-      if (
-        this.paymentForm.controls["takeOut"].value &&
-        this.paymentForm.controls["phoneNumber"].invalid
-      ) {
-        this.errors.push("Invalid phone number");
-        this.showError = true;
-        return;
-      }
+      // if (
+      //   this.paymentForm.controls["takeOut"].value &&
+      //   this.paymentForm.controls["phoneNumber"].invalid
+      // ) {
+      //   this.errors.push("Invalid phone number");
+      //   this.showError = true;
+      //   return;
+      // }
 
       let transaction: any = this.paymentForm.value;
       transaction.ordrGuid = this.ordrGuid;
-      transaction.takeOut = this.paymentForm.controls["takeOut"].value;
-      transaction.specialRequirements = this.paymentForm.controls[
-        "specialRequirements"
-      ].value;
-      transaction.phoneNumber = this.phoneChangeFormat(
-        this.paymentForm.controls["phoneNumber"].value,
-        "db"
-      );
+      // transaction.takeOut = this.paymentForm.controls["takeOut"].value;
+      // transaction.specialRequirements = this.paymentForm.controls[
+      //   "specialRequirements"
+      // ].value;
+      // transaction.phoneNumber = this.phoneChangeFormat(
+      //   this.paymentForm.controls["phoneNumber"].value,
+      //   "db"
+      // );
       //transction.quantity = this.quantity;
-      console.log(transaction);
+      // console.log(transaction);
       this.trnsService.createTransaction(transaction).subscribe((res) => {
         // console.log(res);
         if (res["success"]) {
@@ -147,89 +147,89 @@ export class PaymentWithBalanceComponent implements OnInit {
     this.showSuccessNotification = $event;
   }
 
-  phoneNumberChange(value) {
-    let val = value;
-    if (val.length > 14) {
-      this.paymentForm.controls["phoneNumber"].setValue(
-        val.slice(0, val.length - 1)
-      );
-      return;
-    }
-    let lk = val[val.length - 1];
-    if (this.prevValue.length < val.length) {
-      if (
-        lk &&
-        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(lk)
-      ) {
-        if (val.length == 3) {
-          if (val[0] == "1" || val[0] == "0") {
-            this.paymentForm.controls["phoneNumber"].setValue(val.slice(1));
-          }
-        } else if (val.length == 4) {
-          this.paymentForm.controls["phoneNumber"].setValue(
-            `(${val.slice(0, 3)}) ${val[3]}`
-          );
-        } else if (val.length == 10) {
-          this.paymentForm.controls["phoneNumber"].setValue(
-            `${val.slice(0, 9)}-${val[9]}`
-          );
-        }
-      } else if (lk) {
-        this.paymentForm.controls["phoneNumber"].setValue(
-          val.slice(0, val.length - 1)
-        );
-      }
-      if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(lk)) {
-        this.prevValue = value;
-      }
-    } else {
-      if (val.length == 3) {
-        if (val[0] == "1" || val[0] == "0") {
-          this.paymentForm.controls["phoneNumber"].setValue(val.slice(1));
-        }
-      }
-      if (val[val.length - 1] == " " && val.length == 6) {
-        this.paymentForm.controls["phoneNumber"].setValue(`${val.slice(1, 4)}`);
-        this.prevValue = val.slice(1, 4);
-      } else if (isNaN(val) && val.length <= 4) {
-        this.paymentForm.controls["phoneNumber"].setValue(
-          `${val.replace(/\D/g, "")}`
-        );
-      } else {
-        this.prevValue = this.paymentForm.controls["phoneNumber"].value;
-      }
-    }
-  }
+  // phoneNumberChange(value) {
+  //   let val = value;
+  //   if (val.length > 14) {
+  //     this.paymentForm.controls["phoneNumber"].setValue(
+  //       val.slice(0, val.length - 1)
+  //     );
+  //     return;
+  //   }
+  //   let lk = val[val.length - 1];
+  //   if (this.prevValue.length < val.length) {
+  //     if (
+  //       lk &&
+  //       ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(lk)
+  //     ) {
+  //       if (val.length == 3) {
+  //         if (val[0] == "1" || val[0] == "0") {
+  //           this.paymentForm.controls["phoneNumber"].setValue(val.slice(1));
+  //         }
+  //       } else if (val.length == 4) {
+  //         this.paymentForm.controls["phoneNumber"].setValue(
+  //           `(${val.slice(0, 3)}) ${val[3]}`
+  //         );
+  //       } else if (val.length == 10) {
+  //         this.paymentForm.controls["phoneNumber"].setValue(
+  //           `${val.slice(0, 9)}-${val[9]}`
+  //         );
+  //       }
+  //     } else if (lk) {
+  //       this.paymentForm.controls["phoneNumber"].setValue(
+  //         val.slice(0, val.length - 1)
+  //       );
+  //     }
+  //     if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(lk)) {
+  //       this.prevValue = value;
+  //     }
+  //   } else {
+  //     if (val.length == 3) {
+  //       if (val[0] == "1" || val[0] == "0") {
+  //         this.paymentForm.controls["phoneNumber"].setValue(val.slice(1));
+  //       }
+  //     }
+  //     if (val[val.length - 1] == " " && val.length == 6) {
+  //       this.paymentForm.controls["phoneNumber"].setValue(`${val.slice(1, 4)}`);
+  //       this.prevValue = val.slice(1, 4);
+  //     } else if (isNaN(val) && val.length <= 4) {
+  //       this.paymentForm.controls["phoneNumber"].setValue(
+  //         `${val.replace(/\D/g, "")}`
+  //       );
+  //     } else {
+  //       this.prevValue = this.paymentForm.controls["phoneNumber"].value;
+  //     }
+  //   }
+  // }
 
-  phoneNumberChangeEvent(event) {
-    let val = event.target.value;
-    if (val.length >= 14) {
-      let x = val.search(/(\(\d{3}\))(\s)\d{3}(-)\d{4}/);
-      if (x != -1) {
-        let str = val.slice(x, x + 14);
-        this.paymentForm.controls["phoneNumber"].setValue(str);
-      } else {
-        this.paymentForm.controls["phoneNumber"].setValue("");
-      }
-    }
-  }
+  // phoneNumberChangeEvent(event) {
+  //   let val = event.target.value;
+  //   if (val.length >= 14) {
+  //     let x = val.search(/(\(\d{3}\))(\s)\d{3}(-)\d{4}/);
+  //     if (x != -1) {
+  //       let str = val.slice(x, x + 14);
+  //       this.paymentForm.controls["phoneNumber"].setValue(str);
+  //     } else {
+  //       this.paymentForm.controls["phoneNumber"].setValue("");
+  //     }
+  //   }
+  // }
 
-  preventPaste(event) {
-    event.preventDefault();
-  }
+  // preventPaste(event) {
+  //   event.preventDefault();
+  // }
 
-  phoneChangeFormat(value, type) {
-    if (type == "db") {
-      return "+1" + value.replace(/[()-\s]/g, "");
-    } else {
-      let v = value.replace("+1", "").replace(/[()-\s]/g, "");
-      return `(${v.slice(0, 3)}) ${v.slice(3, 6)}-${v.slice(6)}`;
-    }
-  }
+  // phoneChangeFormat(value, type) {
+  //   if (type == "db") {
+  //     return "+1" + value.replace(/[()-\s]/g, "");
+  //   } else {
+  //     let v = value.replace("+1", "").replace(/[()-\s]/g, "");
+  //     return `(${v.slice(0, 3)}) ${v.slice(3, 6)}-${v.slice(6)}`;
+  //   }
+  // }
 
-  toggleCheckbox() {
-    this.paymentForm.controls["takeOut"].setValue(
-      !this.paymentForm.controls["takeOut"].value
-    );
-  }
+  // toggleCheckbox() {
+  //   this.paymentForm.controls["takeOut"].setValue(
+  //     !this.paymentForm.controls["takeOut"].value
+  //   );
+  // }
 }
--- src/app/components/public-products/public-products.component.ts
+++ src/app/components/public-products/public-products.component.ts
@@ -51,6 +51,7 @@ export class PublicProductsComponent implements OnInit {
     document.documentElement.scrollTop = 0;
 
     this.address = JSON.parse(localStorage.getItem("client_address"));
+    this.searchInput.setValue(this.address.CityName || "Your Location");
     if (!this.address) {
       this.router.navigate(["/"]);
     }
@@ -197,6 +198,7 @@ export class PublicProductsComponent implements OnInit {
 
   addressChanged(address) {
     this.address = address;
+    this.searchInput.setValue(this.address.CityName || "Your Location");
     this.reachedPageEnd = false;
     this.getProducts();
     this.onBlur();
